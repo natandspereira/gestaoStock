dbdbf66881d2e9bb14f8f9915eece7d5c9426345
Natan Pereira <167100852+natandspereira@users.noreply.github.com>
2025-06-30 19:57:36 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-
+<?php 
+// include __DIR__ . '/src/pages/Create_User.php';
+?>
 
 
 
@@ -9,22 +11,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GestãoStock</title>
     <!-- LINK CSS -->
-     <link rel="stylesheet" href="./src/assets/css/index.css">
+    <link rel="stylesheet" href="./src/assets/css/Index.css">
     <!-- LINK FAVICON -->
-     <link rel="shortcut icon" href="./src/assets/img/favicon_logo.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="./src/assets/img/favicon_logo.ico" type="image/x-icon">
+    <!-- LINK ICONS GOOGLE -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 </head>
 <body>
     <!-- FORM LOGIN -->
     <form action="" method="post">
         <div class="container">
             <img src="./src/assets/img/logo.svg" alt="logo">
-            <p>Bem-vindo de volta! Faça login para gerenciar seu estoque</p>
-            <input type="text" name="nome" placeholder="Usuário">
-            <input type="password" name="senha" placeholder="Senha">
-            <span>
+            <p>Bem-vindo! Faça login para gerenciar seu estoque</p>
+            <!-- INPUT USUÁRIO -->
+              <div class="inputUsuario">
+                <label id="nome">
+                    <i class="material-icons" id="iconAccount">account_circle</i>
+                    <input type="text" name="nome" id="nome" placeholder="Usuário">
+                </label>
+              </div>
+            <!-- INPUT SENHA   -->
+              <div class="inputSenha">
+                <label id="senha">
+                    <i class="material-icons" id="iconLock">lock</i>
+                    <input type="password" name="senha" id="senha" placeholder="Senha">
+                </label>
+              </div>  
+            <!-- BOTÃO -->
+            <div class="btnLogin">
                 <button type="submit" id="btnEntrar">entrar</button>
-                <button id="btnCadastro">cadastro</button>
-            </span>
+                <a href="./src/pages/Create_User.php">
+                    <button type="button" id="btnCadastro">cadastro</button>
+                </a>
+            </div>
             <p>Esqueceu sua senha? <a href="#">Recupera sua senha aqui.</a></p>
         </div>
     </form>
