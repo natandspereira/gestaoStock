faa7fe3b6aaff060ebbde32a3ad367d202897568
Natan <contato.natanpereira@gmail.com>
2025-06-30 01:28:52 -0300

    Página de Login Criada

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
-<?php 
 
-?>
+
+
 
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GestãoStock</title>
     <!-- LINK CSS -->
      <link rel="stylesheet" href="./src/assets/css/index.css">
+    <!-- LINK FAVICON -->
+     <link rel="shortcut icon" href="./src/assets/img/favicon_logo.ico" type="image/x-icon">
 </head>
 <body>
-    <!-- LOGIN -->
-    <div class="login">
-        <div class="logo">
+    <!-- FORM LOGIN -->
+    <form action="" method="post">
+        <div class="container">
             <img src="./src/assets/img/logo.svg" alt="logo">
+            <p>Bem-vindo de volta! Faça login para gerenciar seu estoque</p>
+            <input type="text" name="nome" placeholder="Usuário">
+            <input type="password" name="senha" placeholder="Senha">
+            <span>
+                <button type="submit" id="btnEntrar">entrar</button>
+                <button id="btnCadastro">cadastro</button>
+            </span>
+            <p>Esqueceu sua senha? <a href="#">Recupera sua senha aqui.</a></p>
         </div>
-       <div class="userAndPassword">
-             <input type="text" placeholder="Usuário" required>
-             <input type="password" placeholder="Senha" required>
-       </div>
-       <div class="btnLogin">
-            <button id="toEnter">entrar</button>
-            <button id="register">cadastrar</button>
-       </div>
-       <p>Esqueceu sua senha?<a href="#">Recupere sua senha aqui</a></p>
-    </div>
-   
+    </form>
 </body>
 </html>
